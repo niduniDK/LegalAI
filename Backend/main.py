24a9a2b5e25c017f62dbd33446f52478c798dcc7
Kisara Kodithuwakku <kisarakd@gmail.com>
2025-08-28 07:10:28 +0530

    update backend requirements.txt and refactor main.py to use import string

--- Backend/main.py
+++ Backend/main.py
@@ -20,5 +20,4 @@ app.include_router(handle_search.router, prefix="/get_docs")
 app.include_router(get_recommmendations.router, prefix="/recommendations")
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
